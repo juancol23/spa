ef46279993348bbfb20026a874a1db1399417a1a
valdemar <juan.colichon@vasslatam.com>
2018-05-08 23:49:35 -0500

    sprint-1 Ahgregando validacion con correo

--- sendemail.php
+++ sendemail.php
@@ -1,25 +1,53 @@
 <?php
- 
-$para      = 'valdcolra@gmail.com';  
-//
+// Multiple recipients
+$to = 'valdcolra@gmail.com'; // note the comma
 
+// Subject
+$subject = 'Spa OASIS ';
 $nombre   = $_POST['nombre'];
 $email    =  $_POST['email'];
 $mensaje   = $_POST['mensaje'];
 $telefono   = $_POST['telefono'];
-$cabeceras = 'From: oasispucallpa@riva.com' . "\r\n" .
-    'Reply-To: oasispucallpa@riva.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
- 
-    // ."Content-type: text/html; charset=utf-8"
+// Message
+$message = '
+<html>
+<head>
+  <title>Title Spa</title>
+</head>
+<body>
+  <p>El cliente ha solicitado por medio del sitio web una cita al '.$subject.'</p>
+  <table>
+    <tr>
+     <th>Nombre</th> <th>'.$nombre.'</th> 
+    </tr>
+    <tr>
+      <th>Email</th> <td>'.$email.'</td> 
+    </tr>
+    <tr>
+      <th>Mensaje</th> <td>'.$mensaje.'</td> 
+    </tr>
+    <tr>
+      <th>Telefono</th> <td>'.$telefono.'</td> 
+    </tr>
+    <p>Comunicarse Pronto</p>
+  </table>
+</body>
+</html>
+';
 
+// To send HTML mail, the Content-type header must be set
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
-	mail($para, 'Cliente Web ', $body."\r\n" .$nombre."\r\n" .$email."\r\n" .$telefono."\r\n" .$mensaje, $cabeceras); 
-	
-	echo json_encode($data);
-	 
+// Additional headers
+$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
+$headers[] = 'From: OASIS <oasis@example.com>';
+$headers[] = 'Cc: birthdayarchive@example.com';
+$headers[] = 'Bcc: birthdaycheck@example.com';
+
+// Mail it
+mail($to, $subject, $message, implode("\r\n", $headers));
  
+ 
+header('Location: http://oasispucallpa.com');
 ?>
-
-
-
